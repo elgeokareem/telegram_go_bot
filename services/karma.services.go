46c1560e34f211b0abaebed10395acdd0ae5e104
William <elkareem123@gmail.com>
2025-02-21 17:46:04 -0400

    logger 2

--- services/karma.services.go
+++ services/karma.services.go
@@ -193,6 +193,7 @@ func ProcessTelegramMessages(telegramUrl string, token string, offset int, conn 
 		}
 
 		if err := AddKarmaToUser(update, karmaValue, conn); err != nil {
+			fmt.Println("ERROR ADDING KARMA 1", err.Error())
 			errorInput := ErrorRecordInput{
 				SenderID:   update.Message.ReplyToMessage.From.ID,
 				ReceiverID: update.Message.From.ID,
@@ -200,7 +201,7 @@ func ProcessTelegramMessages(telegramUrl string, token string, offset int, conn 
 				Error:      err.Error(),
 			}
 			SendMessageWithReply(chatId, senderMessageId, "Error adding karma")
-			fmt.Println("ERROR ADDING KARMA", err.Error())
+			fmt.Println("ERROR ADDING KARMA 2", err.Error())
 			CreateErrorRecord(conn, errorInput)
 			continue // Skip this update instead of returning
 		}
